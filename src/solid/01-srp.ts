8de87a633f3448b5c5f0031b6dbb67d002a6aa62
Jessica Apolinar Crisostomo <jessica.apolinar@3pillarglobal.com>
2023-10-26 18:38:08 -0600

    solid principles

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -27,18 +27,26 @@
   // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
   // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
   class ProductBloc {
+
+    private productService: ProductService;
+    private mailer: Mailer;
+
+    constructor(productService: ProductService, mailer: Mailer) {
+      this.productService = productService;
+      this.mailer = mailer;
+    }
     loadProduct(id: number) {
       // Realiza un proceso para obtener el producto y retornarlo
-      console.log("Producto: ", { id, name: "OLED Tv" });
+      this.productService.getProduct(id)
     }
 
     saveProduct(product: Product) {
       // Realiza una petición para salvar en base de datos
-      console.log("Guardando en base de datos", product);
+      this.productService.saveProduct(product)
     }
 
     notifyClients() {
-      console.log("Enviando correo a los clientes");
+      this.mailer.sendEmail(['email@mail.com'], "template")
     }
 
     
@@ -56,7 +64,10 @@
     }
   }
 
-  const productBloc = new ProductBloc();
+  const productService = new ProductService()
+  const mailer = new Mailer()
+
+  const productBloc = new ProductBloc(productService, mailer);
   const cartBlock = new CartBlock()
   productBloc.loadProduct(10);
   productBloc.saveProduct({ id: 10, name: "OLED TV" });
